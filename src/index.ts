5a6b6706800fa43231f3e9dcc7514be5f76d2c4c
danielpark <danielpark1140@gmail.com>
2021-06-18 00:35:17 -0400

    Express hello world

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./consts";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
-
+import express from "express";
 const main = async () => {
 	const orm = await MikroORM.init(microConfig);
 	await orm.getMigrator().up();
@@ -10,5 +10,13 @@ const main = async () => {
 	// await orm.em.persistAndFlush(post);
 	// const posts = await orm.em.find(Post, {});
 	// console.log(posts);
+
+	const app = express();
+	app.get("/", (_, res) => {
+		res.send("hello");
+	});
+	app.listen(4000, () => {
+		console.log("Server on localhost:4000");
+	});
 };
 main();
