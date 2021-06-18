9ea8e3dfb87eee8af0acdbaba1c0845847101a6d
danielpark <danielpark1140@gmail.com>
2021-06-18 10:51:21 -0400

    Registration, need to find a hashing pkg

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { Post } from "./entities/Post";
 import { __prod__ } from "./consts";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 require("dotenv").config();
 const pw = process.env.pg_pw;
@@ -11,7 +12,7 @@ export default {
 		path: path.join(__dirname, "./migrations"),
 		pattern: /^[\w-]+\d+\.[tj]s$/,
 	},
-	entities: [Post],
+	entities: [Post, User],
 	dbName: "lireddit",
 	type: "postgresql",
 	user: "postgres",
