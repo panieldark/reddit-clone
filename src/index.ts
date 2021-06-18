b2a5aa1c4bfa3be9d67fc56a7adec674f1039488
danielpark <danielpark1140@gmail.com>
2021-06-18 00:49:44 -0400

    GraphQL server using TS

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { __prod__ } from "./consts";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+
 const main = async () => {
 	const orm = await MikroORM.init(microConfig);
 	await orm.getMigrator().up();
@@ -12,9 +16,15 @@ const main = async () => {
 	// console.log(posts);
 
 	const app = express();
-	app.get("/", (_, res) => {
-		res.send("hello");
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver],
+			validate: false,
+		}),
 	});
+	apolloServer.applyMiddleware({ app });
+
 	app.listen(4000, () => {
 		console.log("Server on localhost:4000");
 	});
